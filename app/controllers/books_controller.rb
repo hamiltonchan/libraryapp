06aeb054811f9e0feabbf76555e5ba9951952da8
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-01-30 10:45:32 -0800

    show function sorta works

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
 
+
 	def index
 		@books = Book.all
+	end
+
+	def show
+		
 	end
 
 	def new
@@ -23,5 +28,5 @@ class BooksController < ApplicationController
 	private
 	def book_params
 		params.require(:book).permit(:title, :author, :year, :isbn, :genre)
-
+	end
 end
